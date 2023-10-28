64cd559c4123d034844cb313446871b446718af2
Gianluca Fuoco <gianluca_1227@hotmail.com>
2023-10-28 15:56:19 -0400

    feat: AutoComplete widget on trainer info

--- ui/trainer_info.go
+++ ui/trainer_info.go
@@ -2,9 +2,9 @@ package ui
 
 import (
     "github.com/geefuoco/trainer_editor/data_objects"
+    "github.com/geefuoco/trainer_editor/custom_widgets"
+    "time"
     "fmt"
-    "reflect"
-    "strconv"
     "strings"
     "fyne.io/fyne/v2"
     "fyne.io/fyne/v2/widget"
@@ -14,20 +14,37 @@ import (
 
 )
 
+var (
+    isProcessing bool
+    processingText string
+    processingTimer *time.Timer
+    throttleInterval = 400 * time.Millisecond
+)
+
 func createTrainerInfo(trainer *data_objects.Trainer) *fyne.Container{ 
     form := container.New(layout.NewFormLayout())
     content := container.NewVBox()
 
-    structValue := reflect.ValueOf(trainer).Elem()
+    trainerClassLabel := widget.NewLabel("Trainer Class")
+    trainerClassSelectBox:= custom_widgets.NewAutoComplete(trainerClasses)
+    trainerClassSelectBox.SetText(trainer.TrainerClass)
+    trainerClassSelectBox.OnChanged = func(value string) {
+        if len(value) < 4 || isProcessing {
+            trainerClassSelectBox.HideCompletion()
+            return
+        }
+        processingText = value
+        processingTimer = time.AfterFunc(throttleInterval, func() {
+            isProcessing = true
+            defer func() {
+                isProcessing = false
+                processingText = ""
+            }()
 
-    for i :=0; i < structValue.NumField(); i++ {
-        field := structValue.Field(i)
-        fieldName := structValue.Type().Field(i).Name
-        if fieldName == "Party" {
-            continue
-        } else if fieldName == "TrainerClass" {
-            label := widget.NewLabel(fieldName)
-            entry:= widget.NewSelect(trainerClasses, func(value string) {
+            filtered := filterOptions(trainerClasses, value)
+            trainerClassSelectBox.SetOptions(filtered)
+            trainerClassSelectBox.ShowCompletion()
+            if SliceContains(trainerClasses, value) {
                 trainer.TrainerClass = value
             }
         })
@@ -58,21 +75,37 @@ func createTrainerInfo(trainer *data_objects.Trainer) *fyne.Container{
 
     // Trainer Pic
     trainerPicLabel := widget.NewLabel("Trainer Pic")
-    trainerPicSelectBox := widget.NewSelectEntry(trainerPics)
+    trainerPicSelectBox := custom_widgets.NewAutoComplete(trainerPics)
     trainerPic = canvas.NewImageFromFile(trainerPicMap[trainer.TrainerPic])
     trainerPic.FillMode = canvas.ImageFillContain
     trainerPic.SetMinSize(fyne.NewSize(64, 64))
     trainerPicSelectBox.SetText(trainer.TrainerPic)
-    trainerPicSelectBox.OnChanged = func(s string) {
-        _, has := trainerPicMap[s]
-        if has {
-            trainer.TrainerPic = s
-            trainerPic = canvas.NewImageFromFile(trainerPicMap[trainer.TrainerPic])
-            trainerPic.FillMode = canvas.ImageFillContain
-            trainerPic.SetMinSize(fyne.NewSize(64, 64))
-            picWrapper.Objects[1] = trainerPic
-            picWrapper.Refresh()
+    trainerPicSelectBox.OnChanged = func(value string) {
+        if len(value) < 3 || isProcessing {
+            trainerClassSelectBox.HideCompletion()
+            return
         }
+        processingText = value
+        processingTimer = time.AfterFunc(throttleInterval, func() {
+            isProcessing = true
+            defer func() {
+                isProcessing = false
+                processingText = ""
+            }()
+
+            filtered := filterOptions(trainerPics, value)
+            trainerPicSelectBox.SetOptions(filtered)
+            trainerPicSelectBox.ShowCompletion()
+            _, has := trainerPicMap[value]
+            if has {
+                trainer.TrainerPic = value
+                trainerPic = canvas.NewImageFromFile(trainerPicMap[trainer.TrainerPic])
+                trainerPic.FillMode = canvas.ImageFillContain
+                trainerPic.SetMinSize(fyne.NewSize(64, 64))
+                picWrapper.Objects[1] = trainerPic
+                picWrapper.Refresh()
+            }
+        })
     }
     trainerPicLabelWrapper := container.New(layout.NewFormLayout(), trainerPicLabel, trainerPicSelectBox)
     picWrapper = container.NewVBox(trainerPicLabelWrapper, trainerPic)
@@ -80,60 +113,117 @@ func createTrainerInfo(trainer *data_objects.Trainer) *fyne.Container{
 
     // Item 0
     itemLabel0 := widget.NewLabel("ITEM 0")
-    itemSelectBox0 := widget.NewSelectEntry(items)
+    itemSelectBox0 := custom_widgets.NewAutoComplete(items)
     form.Add(itemLabel0)
     itemValue := trainer.Items[0]
     itemSelectBox0.SetText(itemValue)
-    itemSelectBox0.OnChanged = func(s string) {
-        if len(s) >= 9 {
-            if SliceContains(items, s) {
-                trainer.Items[0] = s;
-            }
+    itemSelectBox0.OnChanged = func(value string) {
+        if len(value) < len("cut") || isProcessing {
+            trainerClassSelectBox.HideCompletion()
+            return
         }
+        processingText = value
+        processingTimer = time.AfterFunc(throttleInterval, func() {
+            isProcessing = true
+            defer func() {
+                isProcessing = false
+                processingText = ""
+            }()
+
+            filtered := filterOptions(items, value)
+            itemSelectBox0.SetOptions(filtered)
+            itemSelectBox0.ShowCompletion()
+            if SliceContains(items, value) {
+                trainer.Items[0] = value;
+            }
+        })
     }
     form.Add(itemSelectBox0)
 
     // Item 1
     itemLabel1 := widget.NewLabel("ITEM 1")
-    itemSelectBox1 := widget.NewSelectEntry(items)
+    itemSelectBox1 := custom_widgets.NewAutoComplete(items)
     form.Add(itemLabel1)
     itemValue = trainer.Items[1]
     itemSelectBox1.SetText(itemValue)
-    itemSelectBox1.OnChanged = func(s string) {
-        if len(s) >= 9 {
-            if SliceContains(items, s) {
-                trainer.Items[1] = s;
-            }
+    itemSelectBox1.OnChanged = func(value string) {
+        if len(value) < len("cut") || isProcessing {
+            trainerClassSelectBox.HideCompletion()
+            return
         }
+        processingText = value
+        processingTimer = time.AfterFunc(throttleInterval, func() {
+            isProcessing = true
+            defer func() {
+                isProcessing = false
+                processingText = ""
+            }()
+
+            filtered := filterOptions(items, value)
+            itemSelectBox1.SetOptions(filtered)
+            itemSelectBox1.ShowCompletion()
+            if SliceContains(items, value) {
+                trainer.Items[1] = value;
+            }
+        })
     }
     form.Add(itemSelectBox1)
 
     // Item 2
+
     itemLabel2 := widget.NewLabel("ITEM 2")
-    itemSelectBox2 := widget.NewSelectEntry(items)
+    itemSelectBox2 := custom_widgets.NewAutoComplete(items)
     form.Add(itemLabel2)
     itemValue = trainer.Items[2]
     itemSelectBox2.SetText(itemValue)
-    itemSelectBox2.OnChanged = func(s string) {
-        if len(s) >= 9 {
-            if SliceContains(items, s) {
-                trainer.Items[2] = s;
-            }
+    itemSelectBox2.OnChanged = func(value string) {
+        if len(value) < len("cut") || isProcessing {
+            trainerClassSelectBox.HideCompletion()
+            return
         }
+        processingText = value
+        processingTimer = time.AfterFunc(throttleInterval, func() {
+            isProcessing = true
+            defer func() {
+                isProcessing = false
+                processingText = ""
+            }()
+
+            filtered := filterOptions(items, value)
+            itemSelectBox2.SetOptions(filtered)
+            itemSelectBox2.ShowCompletion()
+            if SliceContains(items, value) {
+                trainer.Items[2] = value;
+            }
+        })
     }
     form.Add(itemSelectBox2)
     // Item 3
     itemLabel3 := widget.NewLabel("ITEM 3")
-    itemSelectBox3 := widget.NewSelectEntry(items)
+    itemSelectBox3 := custom_widgets.NewAutoComplete(items)
     form.Add(itemLabel3)
     itemValue = trainer.Items[3]
     itemSelectBox3.SetText(itemValue)
-    itemSelectBox3.OnChanged = func(s string) {
-        if len(s) >= 9 {
-            if SliceContains(items, s) {
-                trainer.Items[3] = s;
-            }
+    itemSelectBox3.OnChanged = func(value string) {
+        if len(value) < len("cut") || isProcessing {
+            trainerClassSelectBox.HideCompletion()
+            return
         }
+        processingText = value
+        processingTimer = time.AfterFunc(throttleInterval, func() {
+            isProcessing = true
+            defer func() {
+                isProcessing = false
+                processingText = ""
+            }()
+
+            filtered := filterOptions(items, value)
+            itemSelectBox3.SetOptions(filtered)
+            itemSelectBox3.ShowCompletion()
+            if SliceContains(items, value) {
+                trainer.Items[3] = value;
+            }
+        })
     }
     form.Add(itemSelectBox3)
     
@@ -244,4 +334,14 @@ func buildTrainerEncounterMusicPath(path string) string {
     buf.WriteString("/constants")
     buf.WriteString("/trainers.h")
     return buf.String()
+}
+
+func filterOptions(options []string, text string) []string {
+    filtered := []string{}
+    for _, option := range options {
+        if strings.Contains(option, text) {
+            filtered = append(filtered, option)
+        }
+    }
+    return filtered
 }
